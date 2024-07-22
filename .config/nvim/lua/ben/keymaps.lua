4906316bd1b962b3b564a36820cd67f8e4852222
Ben Worth <u3243222@uni.canberra.edu.au>
2024-07-22 10:27:13 +1000

    added some keymaps

--- .config/nvim/lua/ben/keymaps.lua
+++ .config/nvim/lua/ben/keymaps.lua
@@ -5,7 +5,10 @@ local keymap = vim.keymap -- for conciseness
 
 keymap.set("n", "<leader>q", "<cmd>bd<CR>", { desc = "Quit once" })
 keymap.set("n", "<leader>Q", "<cmd>qa!<CR>", { desc = "Quit all" })
-keymap.set("n", "<leader>e", "<cmd>Lex<CR>", { desc = "Open explorer in V split" })
+-- keymap.set("n", "<leader>e", "<cmd>Lex<CR>", { desc = "Open explorer in V split" })
+keymap.set("n", "<leader>e", "<cmd>Ex<CR>", { desc = "Open explorer" })
+
+keymap.set("n", "<leader>lz", "<cmd>Lazy<CR>", { desc = "Open Lazy package manager" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
@@ -24,3 +27,7 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+
+keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", { desc = "Next Buffer" })
+keymap.set("n", "<leader>bp", "<cmd>bprevious<CR>", { desc = "Previous Buffer" })
