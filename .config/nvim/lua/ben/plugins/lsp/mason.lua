fb23392dd242c1984cf2c7db07c1d97211b01761
Ben Worth <u3243222@uni.canberra.edu.au>
2024-10-14 09:27:19 +1100

    added alacritty

--- .config/nvim/lua/ben/plugins/lsp/mason.lua
+++ .config/nvim/lua/ben/plugins/lsp/mason.lua
@@ -27,28 +27,28 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "ts_ls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
-        "pyright",
-		"gopls",
+		--       "ts_ls",
+		      "html",
+		      "cssls",
+		--       "tailwindcss",
+		--       "svelte",
+		      "lua_ls",
+		--       "graphql",
+		--       "emmet_ls",
+		--       "prismals",
+		      -- "pyright",
+		-- "gopls",
       },
     })
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
+        -- "prettier", -- prettier formatter
+        -- "stylua", -- lua formatter
+        -- "isort", -- python formatter
         "black", -- python formatter
-        "pylint",
-        "eslint_d",
+        -- "pylint",
+        -- "eslint_d",
       },
     })
   end,
